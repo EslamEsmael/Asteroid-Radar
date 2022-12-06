d03b20b80a86f13ca2107198bf690bda2d9fb185
EslamEsmael <EslamEsmael012@Gmail.com>
2022-12-07 01:16:53 +0200

    Modifying constants

--- app/src/main/java/com/eslam/asteroidradar/domain/utlis/Constants.kt
+++ app/src/main/java/com/eslam/asteroidradar/domain/utlis/Constants.kt
@@ -2,7 +2,7 @@ package com.eslam.asteroidradar.domain.utlis
 
 object Constants {
     const val API_QUERY_DATE_FORMAT = "YYYY-MM-dd"
-    const val DEFAULT_END_DATE_DAYS = 7
+    const val DEFAULT_END_DATE_DAYS = 6
     const val BASE_URL = "https://api.nasa.gov/"
     const val API_KEY = "ANshVe8safldhKUpw7OBR9pEBuSkJpRPJ9b5bf9U"
 }
